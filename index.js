19890c724a580825b57528c733070ff9bd76ed80
leafmoes <leafmoes@gmail.com>
2025-04-10 01:05:28 +0800

    edit cdn

--- index.js
+++ index.js
@@ -6,12 +6,15 @@ hexo.on("generateBefore", () => {
 
 const path = require('path');
 
-hexo.log.d("Test==================================Test")
+const cdn = {
+    jquery: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js",
+    friends: "https://raw.zako.uk/assets/data_services/friends.js",
+}
 
 hexo.extend.tag.register("friends", require("./lib/scripts/friends")(hexo));
 
 hexo.extend.filter.register("theme_inject", function (injects) {
     injects.style.push(path.join(__dirname, "lib/next-tag-plugins.styl"));
-    injects.bodyEnd.raw('jquery-js', '<script defer src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>', {}, {cache: true});
-    injects.bodyEnd.raw('data_services-friends-js', '<script defer src="https://cdn.jsdelivr.net/npm/hexo-next-tag-plugins@latest/lib/assets/data_services/friends.js"></script>', {}, {cache: true});
+    injects.bodyEnd.raw('jquery-js', `<script defer src="${cdn.jquery}"></script>`, {}, {cache: true});
+    injects.bodyEnd.raw('data_services-friends-js', `<script defer src="${cdn.friends}"></script>`, {}, {cache: true});
 });
